1675ecafc12ad4a81a1752ca8df8d985d7e2f719
Alec Brooks <alec.e.brooks@gmail.com>
2018-04-30 15:29:01 +1000

    Remove default CRA stuff, add basic fetching, eslint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const fetchAdditionalGameCount = async (currentAmountOfGames) => {
+  const response = await fetch(`https://www.speedrun.com/api/v1/games?_bulk=yes&max=1000&offset=${currentAmountOfGames}`);
+  const json = await response.json();
+  return json.pagination.size;
+};
+
+const fetchRandomGame = async (numberOfGames) => {
+  const randomGameIndex = Math.floor(Math.random() * numberOfGames);
+  const response = await fetch(`https://www.speedrun.com/api/v1/games?offset=${randomGameIndex}`);
+  const json = await response.json();
+  return json.data[0];
+};
+
+const fetchRandomRecordsForGame = async (gameId) => {
+  const response = fetch(`https://www.speedrun.com/api/v1/games/${gameId}/records?top=1`);
+  const json = await response.json();
+  return json;
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numberOfGames: 12000,
+      name: '',
+      records: [],
+    };
+    this.getNumberOfGames = this.getNumberOfGames.bind(this);
+    this.getRandomWR = this.getRandomWR.bind(this);
+  }
+
+  async getNumberOfGames() {
+    this.setState({
+      numberOfGames: this.state.numberOfGames + await fetchAdditionalGameCount(this.state.numberOfGames),
+    });
+  }
+  async getRandomWR() {
+    const { id, names: { international } } = await fetchRandomGame(this.state.numberOfGames);
+    const records = await fetchRandomRecordsForGame(id);
+    this.setState({
+      records,
+      name: international,
+    });
+  }
   render() {
+    console.log(this.state.records);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+        <p>{this.state.numberOfGames}</p>
+        <button onClick={this.getNumberOfGames}>
+          Get number of Games
+        </button>
+        <p>{this.state.name}</p>
+        <button onClick={this.getRandomWR}>
+          Get Random WR
+        </button>
       </div>
     );
   }
